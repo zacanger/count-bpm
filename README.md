0b9b225e09527dff079a10b0cefe252ca804ea24
zacanger <zac@zacanger.com>
2018-04-02 21:55:56 -0600

    Write the actual thing

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ Tiny CLI for getting an approximate BPM
 
 `npx count-bpm`
 
-Tap on any key for a bit. Hit `ctrl+c` when you're done.
+Tap on the enter/return key for a bit. Hit `ctrl+d` when you're done.
 
 You can also install globally if that's your thing: `npm i -g count-bpm`
 
--- index.js
+++ index.js
@@ -1,7 +1,28 @@
 #!/usr/bin/env node
 
-const main = () => {
-
+if (module.parent) {
+  console.error('Please execute this module.')
+  process.exit(1)
 }
 
-if (!module.parent) main()
+let start
+let hadFirstChunk = false
+let text = ''
+
+process.stdin.resume()
+process.stdin.setEncoding('utf8')
+process.stdin.on('data', (chunk) => {
+  if (!hadFirstChunk) {
+    start = Date.now()
+    hadFirstChunk = true
+  }
+  text += chunk
+})
+
+process.stdin.on('end', () => {
+  const end = Date.now()
+  const beats = text.split('').length
+  const time = end - start
+  const bpm = 60000 * beats / time
+  console.log(bpm.toFixed(2))
+})
