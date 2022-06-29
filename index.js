4e6ec59574014c07351f003bfeb3d9f9d41af168
zacanger <zac@zacanger.com>
2022-06-29 15:28:37 -0600

    chore: updates

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 #!/usr/bin/env node
 
-require('./lib')
+let start
+let end
+let text = ''
+
+process.stdin.resume()
+process.stdin.setEncoding('utf8')
+process.stdin.on('data', (chunk) => {
+  start = start || Date.now()
+  text += chunk
+  end = Date.now()
+})
+
+process.stdin.on('end', () => {
+  const beats = text.split('').length
+  const time = end - start
+  const bpm = (60_000 * beats) / time
+  console.log(bpm.toFixed(2))
+})
